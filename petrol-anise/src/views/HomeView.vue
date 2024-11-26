b14171545fa8a5ffb85875ee511e0a6fc673305d
Adam Hochschild <ahochs@mimi.cs.mcgill.ca>
2024-11-25 20:40:05 -0500

    Initial commit

---
+++ petrol-anise/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="about">
+      <h1 style="font-style: italic;">About Us</h1>
+    <p>
+      Pastiche is a one page print and online publication of short stories. We will begin publishing biweekly editions in mid January and continue accepting submissions on a rolling basis. 
+    </p>
+  </div>
+  
+  </main>
+  
+</template>
+
+
---
+++ petrol-anise/src/views/MissedConnView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="missed-connections">
+    <h1>Missed Connections</h1>
+    <p>
+      
+    </p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
